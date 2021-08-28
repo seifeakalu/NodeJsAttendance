7b65e686895df65332db9ec033ec6f070b1caa6c
Seife Akalu <50824246+seifeakalu@users.noreply.github.com>
2021-08-28 09:09:14 +0300

    Update database.config.js

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "bd2zh4vszmnh7n0lnpob-mysql.services.clever-cloud.com",
-  USER: "ukm0xjuv8hoh6aj7",
-  PASSWORD: "pW9txvwrhf0CXjsjJC9d",
-  DATABASE: "bd2zh4vszmnh7n0lnpob",
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DATABASE: "attendance",
 };
